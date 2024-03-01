68d9add2f115cf86e7a19b562e641dfeeb297c9e
eleanor-park <eleanor_park@brown.edu>
2024-03-01 02:22:41 -0500

    feat: more tests added

--- mock/tests/e2e/Functions.spec.ts
+++ mock/tests/e2e/Functions.spec.ts
@@ -87,7 +87,6 @@ test('if i search or view without a file loaded, i get an error', async ({ page 
   await page.getByLabel("Command input").fill("view");
   await page.getByLabel("Submit").click();
 
-  // too many args
   await page.getByLabel("Command input").click();
   await page.getByLabel("Command input").fill("search 1 2");
   await page.getByLabel("Submit").click();
@@ -99,23 +98,34 @@ test('if i search or view without a file loaded, i get an error', async ({ page 
   expect(count).toBe(2);
 });
 
+<<<<<<< Updated upstream
 
 // test('if i use capital letters, commands still work',
+=======
+test('if i view with invalid arguments, i get an error', async ({ page }) => {
+  await page.getByLabel("Command input").click();
+  await page.getByLabel("Command input").fill("load_file numbers-basic.csv ");
+  await page.getByLabel("Command input").click();
+  await page.getByLabel("Command input").fill("view too many args");
+  await page.getByLabel("Submit").click();
+  await expect(
+    page.getByText("Invalid view arguments! Usage: view.")
+  ).toBeVisible();
+});
+>>>>>>> Stashed changes
 
-// test('i can switch modes and it starts in brief mode', async ({ page }) => {
-//   // start with brief so command: hello is not visible
+// test('if i view i see my loaded csv', async ({ page }) => {
 //   await page.getByLabel("Command input").click();
-//   await page.getByLabel("Command input").fill("hello");
-//   await expect(page.getByText("Invalid Command!")).toBeVisible()
-//   await expect(page.getByText("hello!")).not.toBeVisible()
-
-//   // switch to verbose so 
+//   await page.getByLabel("Command input").fill("load_file numbers-basic.csv false");
 //   await page.getByLabel("Command input").click();
-//   await page.getByLabel("Command input").fill("mode verbose");
+//   await page.getByLabel("Command input").fill("view");
 //   await page.getByLabel("Submit").click();
-//   await expect(page.getByText('Usage: mode <verbose> OR mode <brief>.')).toBeVisible();
+//   await expect(
+//     page.getByLabel("output data")
+//   ).toBeVisible();
 // });
 
+<<<<<<< Updated upstream
 // test('if i switch modes with no argument, it prompts me', async ({ page }) => {
 //   await page.getByLabel("Command input").click();
 //   await page.getByLabel("Command input").fill("mode");
@@ -124,3 +134,83 @@ test('if i search or view without a file loaded, i get an error', async ({ page 
 // });
 
 
+=======
+// test('i can load a new file and see a new file', async ({ page }) => {
+// });
+
+// test('if i search with invalid arguments, i get an error', async ({ page }) => {
+// });
+
+// test('if i search with invalid arguments, i get an error', async ({ page }) => {
+  // no args
+  // too many args
+  // nonesensical args
+// });
+
+// test('i can search a csv by column and not', async ({ page }) => {
+// });
+
+// test('if there are no search results, i get a message', async ({ page }) => {
+// });
+
+// test('i can search by column index and header', async ({ page }) => {
+  // invalid column
+// });
+
+// test('i can load a new file and search it', async ({ page }) => {
+// });
+
+// test('i can load, search, view sequentially', async ({ page }) => {
+// });
+
+
+test('i can switch modes and it starts in brief mode', async ({ page }) => {
+  // start with brief so command: hello is not visible
+  await page.getByLabel("Command input").click();
+  await page.getByLabel("Command input").fill("load_file numbers-basic.csv true");
+  await expect(
+    page.getByText("load_file numbers-basic.csv true")
+  ).not.toBeVisible();
+
+  // switch to verbose
+  await page.getByLabel("Command input").click();
+  await page.getByLabel("Command input").fill("mode verbose");
+  await page.getByLabel("Submit").click();
+  // await expect(
+  //   page.getByText("load_file numbers-basic.csv true")
+  // ).toBeVisible();
+
+  await page.getByLabel("Command input").click();
+  await page.getByLabel("Command input").fill("search value");
+  await page.getByLabel("Submit").click();
+
+  // switch back to brief
+  await page.getByLabel("Command input").click();
+  await page.getByLabel("Command input").fill("mode brief");
+  await page.getByLabel("Submit").click();
+  await expect(
+    page.getByText("search value")
+  ).not.toBeVisible();
+});
+
+test('if i switch modes with wrong arguments, i get an error', async ({ page }) => {
+  // no arguments
+  await page.getByLabel("Command input").click();
+  await page.getByLabel("Command input").fill("mode");
+  await page.getByLabel("Submit").click();
+
+  await page.getByLabel("Command input").click();
+  await page.getByLabel("Command input").fill("mode awesome");
+  await page.getByLabel("Submit").click();
+
+  const count = await page.getByText('Usage: mode <verbose> OR mode <brief>.').count();
+  expect(count).toBe(2)
+});
+
+test("if i use capital letters and add extra whitespace, commands still work", async ({ page }) => {
+  // no arguments
+  await page.getByLabel("Command input").click();
+  await page.getByLabel("Command input").fill("ModE");
+  await page.getByLabel("Submit").click();
+});
+>>>>>>> Stashed changes
