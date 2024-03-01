079a3e3f82fc1ecc87865781ae387f8517ee32de
eleanor-park <eleanor_park@brown.edu>
2024-03-01 14:47:11 -0500

    fix: test run bug

--- mock/tests/unit/basic-example.spec.ts
+++ mock/tests/unit/basic-example.spec.ts
@@ -1,9 +1,18 @@
-import { expect, test } from 'vitest';
-import * as main from '../../src/main';
-import { REPLFunctions } from "../../src/components/REPLFunctions"
+/*
+  Demo: test ordinary Java/TypeScript
+*/
 
-test('commandMap maps command to REPLFunction', () => {
-  const functionMap = REPLFunctions();
-  const loadCommand = functionMap.get("load_csv");
-  expect (loadCommand("numbers-basic.csv false")).toBe('Successfully loaded file from "numbers-basic.csv"!');
-})
+import { expect, test } from "vitest";
+
+// all exports from main will now be available as main.X
+// import * as main from '../mock/src/main';
+import * as main from "../../src/main";
+
+test("is 1 + 1 = 2?", () => {
+  expect(1 + 1).toBe(2);
+});
+
+// Notice how you can test vanilla TS functions using Playwright as well!
+test("main.zero() should return 0", () => {
+  expect(main.zero()).toBe(0);
+});
