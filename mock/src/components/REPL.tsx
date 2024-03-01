0b0e363dc74a0550de9215679ebb21a90493c78c
gdhanda26 <113314719+gdhanda26@users.noreply.github.com>
2024-03-01 13:06:18 -0500

    docs: commented REPL

--- mock/src/components/REPL.tsx
+++ mock/src/components/REPL.tsx
@@ -3,27 +3,23 @@ import "../styles/main.css";
 import { REPLHistory } from "./REPLHistory";
 import { REPLInput } from "./REPLInput";
 import { REPLFunctions } from "./REPLFunctions";
-/* 
-  You'll want to expand this component (and others) for the sprints! Remember 
-  that you can pass "props" as function arguments. If you need to handle state 
-  at a higher level, just move up the hooks and pass the state/setter as a prop.
-  
-  This is a great top level component for the REPL. It's a good idea to have organize all components in a component folder.
-  You don't need to do that for this gearup.
-*/
 
+/**
+ * A high-level component for the REPL user interface. Stores high level data that
+ * is shared between REPLHistory and REPLInput, and sets up the page orientation.
+ *
+ * @returns A set of HTML with the REPLHistory on top and a REPLInput at the bottom.
+ */
 export default function REPL() {
+  // An array of tuples, where one value is a string and the other is a string or 2D string array.
   const [historyList, setHistoryList] = useState<
     [string, string | string[][]][]
   >([]);
-  const [outputMode, setOutputMode] = useState<boolean>(true);
-  const commandMap = REPLFunctions();
+  const [outputMode, setOutputMode] = useState<boolean>(true); // Tracks verbose / brief mode
+  const commandMap = REPLFunctions(); // Allows for developer extensibility
 
   return (
     <div className="repl">
-      {/*This is where your REPLHistory might go... You also may choose to add it within your REPLInput 
-      component or somewhere else depending on your component organization. What are the pros and cons of each? */}
-      {/* TODO: Update your REPLHistory and REPLInput to take in new shared state as props */}
       <REPLHistory history={historyList} outputMode={outputMode} />
       <REPLInput
         history={historyList}
