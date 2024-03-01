6bb2284fa61c22bc2ae7d27e823753fa9c500fdf
gdhanda26 <113314719+gdhanda26@users.noreply.github.com>
2024-03-01 13:04:37 -0500

    docs: commented App

--- mock/src/components/App.tsx
+++ mock/src/components/App.tsx
@@ -4,10 +4,14 @@ import { LoginButton } from "./LoginButton";
 import REPL from "./REPL";
 
 /**
- * This is the highest level component!
+ * This is the highest level component of the webpage. Here, we track whether the user
+ * is logged in, and set up the upper-level page logic accordingly.
+ *
+ * @returns HTML for the Header, with a login button and the REPL towards the bottom.
  */
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+  // Update the size of the header according to login-status.
   let headerHeight = "98vh";
   if (isLoggedIn) {
     headerHeight = "26vh";
@@ -16,6 +20,7 @@ function App() {
     <div className="App">
       <p className="App-header" style={{ height: headerHeight }}>
         <h1 style={{ position: "absolute", top: "0px" }}>Welcome!</h1>
+        {/* Update the Sub-Header according to Login Status. */}
         {isLoggedIn && (
           <h3 style={{ position: "absolute", top: "80px" }}>
             (Enter Your Command Below)
@@ -26,11 +31,10 @@ function App() {
             (Please Login to Continue)
           </h3>
         )}
+        {/* Add the Login Button, and the REPL if logged in. */}
         <LoginButton isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
         {isLoggedIn && <REPL />}
       </p>
     </div>
   );
 }
-
-export default App;
